fe0fac390b0edd963e98d1e06fdf6a5815deedff
CarolBRDev <carolbeatrice@gmail.com>
2024-08-29 20:36:27 -0300

    solução para cálculo de área, perímetro e diagonal de um retângulo

---
+++ ProblRetangulo/ProblRetangulo/ProblRetangulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ProblRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fazer um programa para ler os valores da largura e altura de um retângulo. Em seguida, mostrar na tela o valor de sua área, perímetro e diagonal. A Classe para esta atividade deve conter: Atributos: Largura e Altura;Métodos: CalcularArea() : doubleCalcularPerimetro() : doubleCalcularDiagonal() : double
+
+            Console.WriteLine("Digite o valor da largura: ");
+            double largura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor da altura do retângulo: ");
+            double altura = double.Parse(Console.ReadLine());
+            
+            Retangulo r = new Retangulo(altura, largura);
+            
+            Console.WriteLine($"O valor da área do retângulo é: {r.CalcularArea()}.");
+            Console.WriteLine($"O perímetro do retângulo é: {r.CalcularPerimetro()}.");
+            Console.WriteLine($"O valor da diagonal do retângulo é: {r.CalcularDiagonal()}");
+
+        }
+    }
+}
---
+++ ProblRetangulo/ProblRetangulo/ProblRetangulo/Retangulo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblRetangulo
+{
+    internal class Retangulo
+    {
+        public double Altura;
+        public double Largura;
+
+        public Retangulo (double altura, double largura)
+        {
+            Largura = largura;
+            Altura = altura;
+        }
+
+        public double CalcularArea()
+        {
+            double area = Largura * Altura;
+            return area;
+        }
+
+        public double CalcularPerimetro()
+        {
+           
+            double perimetro = 2 * (Largura + Altura);
+            return perimetro;
+        }
+
+        public double CalcularDiagonal()
+        {
+            
+            double diagonal = Math.Sqrt((Largura * Largura) + (Altura * Altura));
+
+            return diagonal;
+        }
+    }
+}
