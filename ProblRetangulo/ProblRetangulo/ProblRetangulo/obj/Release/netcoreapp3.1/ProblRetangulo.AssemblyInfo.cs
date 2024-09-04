e8f6a7848964d4548cd8205cc547d2c5675505a0
Caroline Ramos <carolbeatrice@gmail.com>
2024-09-04 20:49:38 -0300

    remanejo de arquivos dentro do repositório

--- ProblRetangulo/ProblRetangulo/ProblRetangulo/obj/Release/netcoreapp3.1/ProblRetangulo.AssemblyInfo.cs
+++ ProblRetangulo/ProblRetangulo/ProblRetangulo/obj/Release/netcoreapp3.1/ProblRetangulo.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProblRetangulo")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21a81f9a72d6054e56efe812dba469664d893c65")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProblRetangulo")]
