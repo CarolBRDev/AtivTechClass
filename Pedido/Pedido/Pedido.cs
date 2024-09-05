cb65f42215cd921717362ee88d1ec1fd2db95fa3
Caroline Ramos <carolbeatrice@gmail.com>
2024-09-04 22:20:20 -0300

    Solução para calculo de valor total de pedido, com desconto de acordo com a quantidade de produtos

---
+++ Pedido/Pedido/Pedido.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Text;
+
+namespace Pedido
+{
+    public class Pedido
+    {
+
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double CalcularValorTotal()
+        {
+
+
+            if (Quantidade <= 10)
+            {
+                return Quantidade * Preco;
+            }
+            else if (Quantidade <= 20)
+            {
+                double desconto = Quantidade * Preco * 10 / 100;
+                return Quantidade * Preco - desconto;
+            }
+            else if (Quantidade <= 50)
+            {
+                double desconto = Quantidade * Preco * 20 / 100;
+                return Quantidade * Preco - desconto;
+            }
+            else
+            {
+                double desconto = Quantidade * Preco * 25 / 100;
+                return Quantidade * Preco - desconto;
+
+            }
+
+
+
+        }
+
+
+    }
+}
---
+++ Pedido/Pedido/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Pedido
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Crie um programa que receba o pedido de um produto com nome, preço e quantidade. Considere que de acordo com a quantidade solicitada o comprador receberá um desconto sobre o valor total. O programa deve mostrar na tela o valor total do produto após ser registrado o pedido.
+
+                * Até 10 unidades: valor total ​
+                * de 11 a 20 unidades: 10% de desconto​
+                * de 21 a 50 unidades: 20% de desconto ​
+                * acima de 50 unidades: 25% de desconto
+             */
+
+            Pedido pedido = new Pedido();
+            
+            Console.WriteLine("Digite o nome do produto: ");
+            pedido.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o valor unitario do produto: ");
+            pedido.Preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade de produtos: ");
+            pedido.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Valor total do pedido: R${pedido.CalcularValorTotal()}.");
+
+
+
+        }
+    }
+}
